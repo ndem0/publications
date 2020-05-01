b1d5c77dc58e1845667d2175b710aa130e05784e
Nicola Demo <demo.nicola@gmail.com>
2020-05-01 15:12:25 +0200

    Fix bibitems, add json workflow

---
+++ scripts/bib2json.py
@@ -0,0 +1,40 @@
+import re
+import os
+import json
+
+json_list = []
+
+for filename in os.listdir('bibitems'):
+    if not filename.endswith('.bib'):
+        continue
+
+    with open(os.path.join('bibitems', filename), 'r') as f:
+        content = f.read()
+
+    #content = map(lambda x: x.strip(), content)
+    #print(content)
+    result = dict()
+    m = re.search('@.+{(.+?),', content)
+    if m:
+        found = m.group(1)
+    result['bibitem'] = 'https://raw.githubusercontent.com/ndem0/pubblications/master/bibitems/{}.bib'.format(found)
+    content = content[content.find('\n'):]
+    m = re.findall('(.+?)=(.*?),\n', content, flags=re.DOTALL)
+
+    for key, value in m:
+        if key and value: 
+            result[key.strip().lower()] = value.replace('{', '').replace('}', '')
+
+    author = result['author']
+    author_str = ''
+    for name in author.split(' and '):
+        name = name.split()
+        author_str += '{}. {}, '.format(name[0][0].upper(), name[1])
+    author_str = author_str[:-2]
+    result['author'] = author_str
+
+    json_list.append(result)
+
+with open('publications.json', 'w') as f:
+    json.dump(json_list, f)
+
